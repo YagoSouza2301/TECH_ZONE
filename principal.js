5a9382692cc40b3381515734ec753643408f9dea
Yago Souza <patyago29@gmail.com>
2026-08-16 17:49:06 -0300

    a
    a

--- principal.js
+++ principal.js
@@ -1,10 +1,12 @@
 let trilho = document.getElementById('trilho')
 let body = document.querySelector('body')
 let header = document.querySelector('header')
+let footer = document.querySelector('footer')
 
 trilho.addEventListener('click', ()=>{
     trilho.classList.toggle('dark')
     body.classList.toggle('dark')
     header.classList.toggle('dark')
+    footer.classList.toggle('dark')
     document.header.classList.toggle("dark");
 })
