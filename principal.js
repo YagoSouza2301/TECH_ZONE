d8b8a474690adbed2d227b2168b52141bc636c0e
Yago Souza <patyago29@gmail.com>
2026-08-17 17:48:36 -0300

    terminando a primeira sessão

--- principal.js
+++ principal.js
@@ -1,3 +1,5 @@
+// Ideias pegas do Chatgpt
+
 let trilho = document.getElementById('trilho')
 let body = document.querySelector('body')
 let header = document.querySelector('header')
@@ -10,3 +12,39 @@ trilho.addEventListener('click', ()=>{
     footer.classList.toggle('dark')
     document.header.classList.toggle("dark");
 })
+
+// ===================================
+
+const contadores = document.querySelectorAll('.box-numero');
+
+contadores.forEach((contador) => {
+    const valorFinal = Number(contador.dataset.target);
+    let valorAtual = 0;
+
+    const intervalo = setInterval(() => {
+        valorAtual++;
+
+        contador.textContent = valorAtual;
+
+        if (valorAtual >= valorFinal) {
+            clearInterval(intervalo);
+        }
+    }, 20);
+});
+// ===================================
+
+const caixas = document.querySelectorAll('.transparente');
+
+const observer = new IntersectionObserver((entradas) => {
+    entradas.forEach((entrada) => {
+        if (entrada.isIntersecting) {
+            entrada.target.classList.add('aparecer');
+        }
+    });
+}, {
+    threshold: 0.2
+});
+
+caixas.forEach((caixa) => {
+    observer.observe(caixa);
+});
