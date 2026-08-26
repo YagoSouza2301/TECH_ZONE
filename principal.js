69fe9df70a3e0fa319747b9f80948ad979d77841
Yago Souza <patyago29@gmail.com>
2026-08-26 09:36:15 -0300

    Continuar mais tarde

--- principal.js
+++ principal.js
@@ -4,12 +4,15 @@ let trilho = document.getElementById('trilho')
 let body = document.querySelector('body')
 let header = document.querySelector('header')
 let footer = document.querySelector('footer')
+let section = document.querySelector('section')
 
 trilho.addEventListener('click', ()=>{
     trilho.classList.toggle('dark')
     body.classList.toggle('dark')
     header.classList.toggle('dark')
     footer.classList.toggle('dark')
+    section.classList.toggle('dark')
+
     document.header.classList.toggle("dark");
 })
 
